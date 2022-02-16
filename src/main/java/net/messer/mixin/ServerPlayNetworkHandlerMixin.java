59fb06a3f197df92475fa31a5a0677fbbf82e8f0
enjarai <enjarai123@gmail.com>
2022-02-16 15:42:26 +0100

    uploading some WIP stuff

--- src/main/java/net/messer/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/net/messer/mixin/ServerPlayNetworkHandlerMixin.java
@@ -2,6 +2,7 @@ package net.messer.mixin;
 
 import net.messer.mystical_index.util.BigStack;
 import net.messer.mystical_index.util.LibraryIndex;
+import net.messer.mystical_index.util.RequestHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
@@ -33,16 +34,20 @@ public class ServerPlayNetworkHandlerMixin {
         String message = packet.getChatMessage();
 
         // player.getWorld().getBlockState() TODO
+
         server.execute(() -> {
             LibraryIndex index = LibraryIndex.get(player.getWorld(), player.getBlockPos());
             if (!index.isEmpty()) {
+                var itemAmount = RequestHelper.processRequest(index, message);
+                if (itemAmount != null)
+                    player.sendMessage(itemAmount.item().getName().shallowCopy().append(": " + itemAmount.amount()), false);
+
                 for (BigStack bigStack : index.getItems().getAll()) {
                     MutableText tooltipEntry = bigStack.getItem().getName().shallowCopy();
                     tooltipEntry.append(" x").append(String.valueOf(bigStack.getAmount()));
                     player.sendMessage(tooltipEntry, false);
                 }
                 player.sendMessage(new LiteralText("yes"), false);
-                info.cancel();
             }
         });
         // info.cancel();
--- src/main/java/net/messer/mystical_index/block/entity/IndexBlockEntity.java
+++ src/main/java/net/messer/mystical_index/block/entity/IndexBlockEntity.java
@@ -1,22 +1,33 @@
 package net.messer.mystical_index.block.entity;
 
 import eu.pb4.polymer.api.utils.PolymerObject;
+import eu.pb4.sgui.api.elements.BookElementBuilder;
+import eu.pb4.sgui.api.gui.BookGui;
+import eu.pb4.sgui.virtual.book.BookScreenHandler;
 import net.messer.mystical_index.screen.LibraryInventoryScreenHandler;
+import net.messer.mystical_index.util.LibraryIndex;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.gui.screen.ingame.BookEditScreen;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.LecternScreenHandler;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
+import org.lwjgl.system.CallbackI;
 
 public class IndexBlockEntity extends BlockEntity implements NamedScreenHandlerFactory, PolymerObject {
 
     public IndexBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.LIBRARY_BLOCK_ENTITY,pos, state);
+    }
+
+    private LibraryIndex getIndex() {
+        return LibraryIndex.get(getWorld(), getPos());
     }
 
     @Override
@@ -27,6 +38,12 @@ public class IndexBlockEntity extends BlockEntity implements NamedScreenHandlerF
     @Nullable
     @Override
     public ScreenHandler createMenu(int syncId, PlayerInventory inv, PlayerEntity player) {
-        return new LibraryInventoryScreenHandler(syncId,inv,this); // WIP
+        BookElementBuilder bookBuilder = new BookElementBuilder().signed();
+        // TODO iterate over index with i / maxpages and mb add title or smt
+
+        BookGui gui = new BookGui(player);
+
+        return new BookScreenHandler()
+        //return new LibraryInventoryScreenHandler(syncId,inv,this); // WIP
     }
 }
